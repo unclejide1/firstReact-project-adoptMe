26312087e3250afe532c4f1827a01c59e0cf201a
Jideofor Nwoke <njusticej@gmail.com>
2019-12-22 05:33:42 +0100

    add routes to app

--- src/app.js
+++ src/app.js
@@ -1,14 +1,19 @@
 import React from "react";
 import { render } from "react-dom";
+import { Router } from "@reach/router";
 // import Pet from "./Pet";
 import SearchParams from "./searchParams";
+import Details from "./details";
 
 const App = () => {
   return (
     <React.StrictMode>
       <div>
         <h1 id="something-important">Adopt me</h1>
-        <SearchParams />
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
       </div>
     </React.StrictMode>
   );
