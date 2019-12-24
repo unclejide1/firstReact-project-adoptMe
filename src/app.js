a36544c001994624842c150051302916865f24f9
Jideofor Nwoke <njusticej@gmail.com>
2019-12-24 23:53:27 +0100

    update pet.js, index.html, details.js, searchParams.js

--- src/app.js
+++ src/app.js
@@ -1,22 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { render } from "react-dom";
 import { Router, Link } from "@reach/router";
 // import Pet from "./Pet";
 import SearchParams from "./searchParams";
 import Details from "./details";
+import ThemeContext from "./ThemeContext";
 
 const App = () => {
+  const themeHook = useState("darkblue");
   return (
     <React.StrictMode>
-      <div>
-        <header>
-          <Link to="/">Adopt me!</Link>
-        </header>
-        <Router>
-          <SearchParams path="/" />
-          <Details path="/details/:id" />
-        </Router>
-      </div>
+      <ThemeContext.Provider value={themeHook}>
+        <div>
+          <header>
+            <Link to="/">Adopt me!</Link>
+          </header>
+          <Router>
+            <SearchParams path="/" />
+            <Details path="/details/:id" />
+          </Router>
+        </div>
+      </ThemeContext.Provider>
     </React.StrictMode>
   );
 };
